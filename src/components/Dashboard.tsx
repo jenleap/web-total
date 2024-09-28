8ad6f508208c6731636ecb4ee91f74c333eb009f
jenleap <jenleap@gmail.com>
2024-09-28 09:52:58 -0400

    set up router

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./NavBar"
+
+export const Dashboard = () => {
+    return (
+        <>
+            <h1>DASHBOARD</h1>
+            <Navbar />
+        </>
+    )
+}
---
+++ src/components/recipes/RecipeMain.tsx
@@ -0,0 +1,5 @@
+export const RecipeMain = () => {
+    return (
+        <div>RECIPE MAIN</div>
+    )
+}
